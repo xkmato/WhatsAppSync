2826bae71e5644a848f6fb24a4f08e0217e484fd
Nicholas Turner <mugtrade@gmail.com>
2019-02-08 22:07:09 +0300

    Add api auth login

--- WhatsAppSync/settings.py
+++ WhatsAppSync/settings.py
@@ -129,5 +129,12 @@ REST_FRAMEWORK = {
     ),
     'DEFAUTL_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': (
+        'rest_framework.pagination.PageNumberPagination'
+    ),
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ),
+    'PAGE_SIZE': 150,
 }
--- sync/views.py
+++ sync/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
-from rest_framework.response import Response
 from django.db.models import Q
 import datetime
 from .models import Log, Server, Contact, Attachment, Message, Workspace, RapidProMessages, timedelta
 from .serializers import MessageSerializer, ContactSerializer
 from django.http import HttpResponse
+from django.contrib.auth import authenticate
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.authtoken.models import Token
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.status import (
+    HTTP_400_BAD_REQUEST,
+    HTTP_404_NOT_FOUND,
+    HTTP_200_OK
+)
+from rest_framework.response import Response
 
 
 def download_attach(request):
@@ -108,3 +118,21 @@ class ContactView(APIView):
 
         serializer = ContactSerializer(cc, many=True)
         return Response(serializer.data)
+
+
+@csrf_exempt
+@api_view(["POST"])
+@permission_classes((AllowAny,))
+def login(request):
+    username = request.data.get("username")
+    password = request.data.get("password")
+    if username is None or password is None:
+        return Response({'error': 'Please provide both username and password'},
+                        status=HTTP_400_BAD_REQUEST)
+    user = authenticate(username=username, password=password)
+    if not user:
+        return Response({'error': 'Invalid Credentials'},
+                        status=HTTP_404_NOT_FOUND)
+    token, _ = Token.objects.get_or_create(user=user)
+    return Response({'token': token.key},
+                    status=HTTP_200_OK)

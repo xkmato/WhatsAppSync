118310425811e4009ad1f643e1e829c6779f69a6
Nicholas Turner <mugtrade@gmail.com>
2019-02-08 22:08:49 +0300

    Sanitize api url

--- sync/urls.py
+++ sync/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     url(r'^get/$', views.get_rapidpro_messages),
     url(r'^archive/$', views.archive_rapidpro),
     url(r'^delete/$', views.delete_rapidpro),
-    url(r'^api/auth/login/$', views.login),
+    url(r'^api/auth/login/', views.login),
     url(r'^api/messages/(\d+)', views.MessageView.as_view()),
     url(r'^api/contacts/(\d+)$', views.ContactView.as_view()),
 
